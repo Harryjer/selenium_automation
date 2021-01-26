e6e190325531b97f1c50568541ff3623b5d1a325
Thisal Kodithuwakku <33801630+Harryjer@users.noreply.github.com>
2021-01-26 11:11:02 +0530

    Adding config.cs file

---
+++ selenium_automation/Config.cs
@@ -0,0 +1,21 @@
+﻿
+
+namespace selenium_automation
+{
+    public static class Config
+    {
+        public static string BaseUrl = "https://testing.todorvachev.com/";
+
+        public static class Credentials
+        {
+            public static class valid
+            {
+
+            }
+            public static class invalid
+            {
+
+            }
+        }
+    }
+}
